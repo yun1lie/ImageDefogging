5a4a5abdc5098893732cff49b428759622c5602f
rotten <1132264073@qq.com>
2023-04-13 16:49:06 +0800

    home

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,13 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes: [
     {
       path: "/",
       component: () => import("@/views/Home.vue"),
+    },
+    {
+      path: "/login",
+      component: () => import("@/views/login.vue"),
     },
   ],
 });
---
+++ web/src/views/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-form :model="loginForm" ref="loginForm" :rules="loginRules" label-width="80px">
+    <el-form-item label="用户名" prop="username">
+      <el-input v-model="loginForm.username" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input type="password" v-model="loginForm.password" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="login">登录</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    login() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // 执行登录逻辑
+        } else {
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
