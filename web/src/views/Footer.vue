20d945416fd9e24b5e5b42abfd7cb164fbbf0303
rotten <1132264073@qq.com>
2023-04-19 17:15:04 +0800

    优化footer.vue

--- web/src/views/Footer.vue
+++ web/src/views/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-  <el-footer height="80px">
-    <div class="bottom-container">
-      <div class="system-info">
+  <el-footer class="footer">
+    <div class="container">
+      <section class="system-info">
         <h3>系统说明</h3>
         <p>
           该系统使用暗通道去雾算法对腹腔镜图像进行去雾处理，提高图像清晰度。
         </p>
-      </div>
-      <div class="terms">
+      </section>
+      <section class="terms">
         <h3>服务条款及隐私政策</h3>
         <ul>
           <li>本系统严格遵守国家法律法规，保护用户个人信息安全。</li>
@@ -18,58 +18,90 @@
             <li>不得侵犯他人权益。</li>
           </ul>
         </ul>
-      </div>
-      <div class="contact">
+      </section>
+      <section class="contact">
         <h3>联系方式</h3>
-        <p>邮箱：support@example.com</p>
-        <p>电话：010-12345678</p>
-      </div>
+        <address>
+          <p>
+            邮箱：<a href="mailto:support@example.com">support@example.com</a>
+          </p>
+          <p>电话：<a href="tel:010-12345678">010-12345678</a></p>
+        </address>
+      </section>
     </div>
   </el-footer>
 </template>
 
 <style scoped>
-.bottom-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.footer {
+  background-color: #f5f5f5;
+  padding: 20px 0;
+}
+
+.container {
   max-width: 1200px;
   margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.system-info,
+.terms,
+.contact {
+  flex-basis: 30%;
+  margin-bottom: 20px;
 }
 
 .system-info h3,
 .terms h3,
 .contact h3 {
-  margin-bottom: 10px;
+  margin: 0 0 10px;
   font-weight: bold;
-  font-size: 18px;
+  font-size: 1.2rem;
 }
 
 .system-info p,
-.terms ul li {
-  font-size: 14px;
-  line-height: 20px;
+.terms ul li,
+.contact p {
+  font-size: 1rem;
+  line-height: 1.5;
 }
 
-.terms ul {
+.terms ul,
+.terms ul ul {
   list-style: none;
-  padding-left: 0;
-  margin-top: 10px;
+  margin: 0;
+  padding: 0;
 }
 
 .terms ul ul {
-  list-style: disc;
   margin-top: 5px;
   margin-left: 20px;
 }
 
-.contact p {
-  font-size: 14px;
-  line-height: 20px;
-  margin-top: 5px;
+.terms ul li:before {
+  content: "•";
+  margin-right: 5px;
+  color: #999;
+}
+
+.contact p a {
+  color: #666;
+}
+
+.contact p a:hover {
+  color: #333;
+  text-decoration: none;
 }
 
 .contact p:first-of-type {
   margin-top: 0;
 }
 </style>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
